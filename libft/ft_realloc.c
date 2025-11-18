4bbe9783501d09f3f91fc8339b18294d1a5baaa8
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-11-18 16:45:38 +0100

    Add ft_realloC
    Implement ft_realloc function for dynamic memory reallocation.

---
+++ libft/ft_realloc.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavlopo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 15:57:52 by apavlopo          #+#    #+#             */
+/*   Updated: 2025/02/27 16:02:02 by apavlopo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+	void	*new_ptr;
+	size_t	i;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+		return (malloc(new_size));
+
+	if (new_size <= old_size)
+		return (ptr);
+
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+		
+	i = 0;
+	while (i < old_size)
+	{
+		((unsigned char *)new_ptr)[i] = ((unsigned char *)ptr)[i];
+		i++;
+	}
+	free(ptr);
+	return (new_ptr);
+}
