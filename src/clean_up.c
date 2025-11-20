c68dcb9a803b2c9ee5d89ded30d0b643b6f672bb
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-11-20 14:17:14 +0100

    Enhance memory management in clean_up.c
    Added memory cleanup for map and color textures in clean_up.c.

--- src/clean_up.c
+++ src/clean_up.c
@@ -7,8 +7,10 @@ void    clean_up(t_cub *cub){
         if (!cub->map)
             return ;
         if (cub->map)
-            {;} // todo free grid etc.
-        free(cub->map);
+        {
+            free_array(cub->map->grid, cub->map->height);
+            free(cub->map);        
+        }
         if (cub->img)
             mlx_destroy_image(cub->mlx, cub->img);
         if (cub->win)
@@ -18,10 +20,37 @@ void    clean_up(t_cub *cub){
             mlx_destroy_display(cub->mlx);
             free(cub->mlx);
         }
+        if(cub->col)
+        {
+            if(cub->col->no_tex_p)
+                free(cub->col->no_tex_p);
+            if(cub->col->so_tex_p)
+                free(cub->col->so_tex_p);
+            if(cub->col->we_tex_p)
+                free(cub->col->we_tex_p);
+            if(cub->col->ea_tex_p)
+                free(cub->col->ea_tex_p);           
+            free(cub->col);
+        }
     }
 }
 
-void    free_array2(char **arr)
+void    free_array(char **arr, int grid_height)
+{
+    int i;
+   
+    i = 0;
+    if (!arr) 
+        return;
+    while(i < grid_height)
+    {
+        free(arr[i]);
+        i++;
+    }
+    free(arr);
+}
+
+void    free_array2(char **arr) //only for usage when calling with array on stack: error_exit(cub, "Error\nMalloc failed!\n", (char*[]){trimd, NULL});
 {
     int i;
    
