f908e0b0cf07b67a32c3b094a067508448909d79
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-11-25 16:00:46 +0100

    Refactor cub3d.h for structure and type updates
    added s_move, s_dir and turned s_pos parameters to double because the player steps inside the tile not tile by tile

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -5,7 +5,7 @@
 # define WIDTH 1280 //todo or define dynamically with size fun?
 # define HEIGHT 720
 
-# define CUBE_SIZE = 64
+# define CUBE_SIZE 64
 # define PLAYER_HEIGHT (CUBE_SIZE / 2)
 
 //# define PI 3.14159265359
@@ -18,6 +18,8 @@
 # define LEFT 65361
 # define RIGHT 65363
 
+# include <X11/X.h>
+# include <X11/keysym.h>
 #include <stdio.h>
 # include "../gnl/get_next_line.h"
 # include "../libft/ft_printf.h"
@@ -41,10 +43,10 @@ typedef struct s_mlx_data
 
 typedef struct s_rgb
 {
-    int					r;
-    int					g;
-    int					b;
-}						t_rgb;
+    int				r;
+    int				g;
+    int				b;
+}					t_rgb;
 
 typedef struct s_col
 {
@@ -52,23 +54,43 @@ typedef struct s_col
     char		    *so_tex_p;
     char			*we_tex_p;
     char			*ea_tex_p;
+    int             floor_color;
+    int             ceil_color;
     struct s_rgb	floor;
     struct s_rgb	ceil;
     int has_floor;
     int has_ceil;
+      
 }					t_col;
 
 typedef struct s_map {
-    int  **grid;
-    int  widths;  // width[y] gives width of row y
+    char  **grid;
+    int  width;  // width[y] gives width of row y
     int  height;
     int  capacity;
-} t_map;
+}                   t_map;
 
-typedef struct s_pos{
-    int x;
-    int y;
-}t_pos;
+typedef struct s_pos {
+    double x;
+    double y;
+}                   t_pos;
+
+typedef struct s_dir {
+    double dir_x;
+    double dir_y;
+    double plane_x;
+    double plane_y;
+}                   t_dir;
+
+typedef struct s_move
+{
+    int forward;
+    int backward;
+    int left;
+    int right;
+    int rotate_left;
+    int rotate_right;
+}                   t_move;
 
 typedef struct s_cub
 {
@@ -81,11 +103,13 @@ typedef struct s_cub
     struct s_col    *col;
     char            spawn_dir;
     struct s_pos    player_pos;
+    struct s_move   move;
 }					t_cub;
 
 void	make_window(t_cub *cub);
 void    clean_up(t_cub *cub);
-void	error_exit(t_cub *cub, char *msg);
+void	error_exit(t_cub *cub, char *msg, char** to_be_freed);
 int     parse_file(char* filename, t_cub* cub);
+void    free_array(char **arr, int grid_height);
 
 #endif //INC_42_09_CUBE3D_CUBE3D_H
