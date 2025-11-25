4bdf64f2e8eb9073ec7193493352c04d333ebbfd
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-11-25 16:18:17 +0100

    Refactor clean_up function and move free_array

--- src/clean_up.c
+++ src/clean_up.c
@@ -1,16 +1,24 @@
 #include"../inc/cub3d.h"
 
+void    free_array(char **arr, int grid_height)
+{
+    int i;
+   
+    i = 0;
+    if (!arr) 
+        return;
+    while(i < grid_height)
+    {
+        free(arr[i]);
+        i++;
+    }
+    free(arr);
+}
+
 void    clean_up(t_cub *cub){
     {
         if (!cub)
-            return ;
-        if (!cub->map)
-            return ;
-        if (cub->map)
-        {
-            free_array(cub->map->grid, cub->map->height);
-            free(cub->map);        
-        }
+            return ;        
         if (cub->img)
             mlx_destroy_image(cub->mlx, cub->img);
         if (cub->win)
@@ -19,6 +27,11 @@ void    clean_up(t_cub *cub){
         {
             mlx_destroy_display(cub->mlx);
             free(cub->mlx);
+        }
+        if (cub->map)
+        {
+            free_array(cub->map->grid, cub->map->height);
+            free(cub->map);        
         }
         if(cub->col)
         {
@@ -33,21 +46,6 @@ void    clean_up(t_cub *cub){
             free(cub->col);
         }
     }
-}
-
-void    free_array(char **arr, int grid_height)
-{
-    int i;
-   
-    i = 0;
-    if (!arr) 
-        return;
-    while(i < grid_height)
-    {
-        free(arr[i]);
-        i++;
-    }
-    free(arr);
 }
 
 void    free_array2(char **arr) //only for usage when calling with array on stack: error_exit(cub, "Error\nMalloc failed!\n", (char*[]){trimd, NULL});
