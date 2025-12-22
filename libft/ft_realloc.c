bf39b3e3bfd1b4483140c8fd62eadea9cd62548a
Maria Diederich <maria.dieda@gmail.com>
2025-12-22 15:47:06 +0100

    libft corrected one file

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -24,14 +24,11 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 	}
 	if (!ptr)
 		return (malloc(new_size));
-
 	if (new_size <= old_size)
 		return (ptr);
-
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
-		
 	i = 0;
 	while (i < old_size)
 	{
